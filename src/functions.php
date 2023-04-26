c3032eedce2cd539419c26efd4e9b9984ddb428c
Nickolas Burr <nickolasburr@gmail.com>
2023-04-25 22:50:42 -0400

    Use explicit octal notation

--- src/functions.php
+++ src/functions.php
@@ -9,8 +9,11 @@ declare(strict_types=1);
 
 namespace Fcache;
 
+use function is_dir;
+use function mkdir;
+
 const CACHE_DIR = '/tmp/.fcache';
-const DIR_OCTAL = 700;
+const DIR_OCTAL = 0o700;
 const MAX_BYTES = 1024;
 const READ_ONLY = 'r';
 const WRITE_ONLY = 'w';
@@ -20,5 +23,9 @@ const WRITE_ONLY = 'w';
  * @return Cache
  */
 function cache(string $cacheDir = CACHE_DIR): Cache {
+    if (!is_dir($cacheDir)) {
+        mkdir($cacheDir, DIR_OCTAL);
+    }
+
     return new Cache($cacheDir);
 }
