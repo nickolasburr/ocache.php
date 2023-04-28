3f19086be954f81ebb828131dbf092328664dbf5
Nickolas Burr <nickolasburr@gmail.com>
2023-04-27 23:48:23 -0400

    Add StreamHandlerException class

---
+++ src/Exception/StreamHandlerException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * StreamHandlerException.php
+ *
+ * @package     VfsCache\Exception
+ * @copyright   Copyright (C) 2023 Nickolas Burr <nickolasburr@gmail.com>
+ */
+declare(strict_types=1);
+
+namespace VfsCache\Exception;
+
+class StreamHandlerException extends \Exception
+{
+}
