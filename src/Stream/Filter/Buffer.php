c87898bb3f182e42716e4e7f932702c9dc7460b0
Nickolas Burr <nickolasburr@gmail.com>
2023-05-17 00:06:32 -0400

    Create stream filter Buffer class

---
+++ src/Stream/Filter/Buffer.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Buffer.php
+ *
+ * @package     VfsCache\Stream\Filter
+ * @copyright   Copyright (C) 2023 Nickolas Burr <nickolasburr@gmail.com>
+ */
+declare(strict_types=1);
+
+namespace VfsCache\Stream\Filter;
+
+use Fiber;
+use Stringable;
+
+use function array_pop;
+use function array_reverse;
+use function implode;
+use function mb_strlen;
+
+final class Buffer implements Stringable
+{
+    /**
+     * @param string[] $buffer
+     * @param int $length
+     * @return void
+     */
+    public function __construct(
+        private array $buffer = [],
+        private int $length = 0
+    ) {}
+
+    /**
+     * @return string[]
+     */
+    public function getBuffer(): array
+    {
+        return $this->buffer;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    /**
+     * @param string $values
+     * @return static
+     */
+    public function append(string ...$values): static
+    {
+        /** @var string $value */
+        foreach ($values as $value) {
+            $this->buffer[] = $value;
+            $this->length += mb_strlen($value);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function flush(): string
+    {
+        /** @var string $result */
+        $result = '';
+
+        /** @var Fiber|null $fiber */
+        $fiber = Fiber::getCurrent();
+
+        /** @var string[] $buffer */
+        $buffer = array_reverse($this->buffer);
+
+        do {
+            /** @var string|null $chunk */
+            $chunk = array_pop($buffer);
+
+            if ($chunk === null) {
+                break;
+            }
+
+            $result .= (
+                $fiber ? Fiber::suspend($chunk) : $chunk
+            );
+        } while (true);
+
+        $this->reset();
+        return $result;
+    }
+
+    /**
+     * @return void
+     */
+    public function reset(): void
+    {
+        $this->buffer = [];
+        $this->length = 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString(): string
+    {
+        return implode('', $this->buffer);
+    }
+}
