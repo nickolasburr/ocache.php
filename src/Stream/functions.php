672f5c55ad6a6d4717cc9ad26600c66bc3b384b9
Nickolas Burr <nickolasburr@gmail.com>
2023-05-17 00:11:59 -0400

    Add filterRegistry function

--- src/Stream/functions.php
+++ src/Stream/functions.php
@@ -10,3 +10,21 @@ declare(strict_types=1);
 namespace VfsCache\Stream;
 
 const PROTO_VFS = 'vfs';
+
+/**
+ * @param string $name
+ * @param string|object $type
+ * @return FilterRegistry
+ */
+function filterRegistry(
+    string $name = Filter::FILTER_NAME,
+    string|object $type = Filter::class
+): FilterRegistry {
+    static $registries;
+    $registries ??= [];
+
+    /** @var FilterRegistry|null $registry */
+    $registry =& $registries[$name];
+    $registry ??= (new FilterRegistry())->register($name, $type);
+    return $registry;
+}
