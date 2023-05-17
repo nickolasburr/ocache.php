1d00aea5b80ac1fc17e4549924d1e797a7333c2c
Nickolas Burr <nickolasburr@gmail.com>
2023-05-16 23:08:09 -0400

    Create StreamFilterException class

---
+++ src/Exception/StreamFilterException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * StreamFilterException.php
+ *
+ * @package     VfsCache\Exception
+ * @copyright   Copyright (C) 2023 Nickolas Burr <nickolasburr@gmail.com>
+ */
+declare(strict_types=1);
+
+namespace VfsCache\Exception;
+
+class StreamFilterException extends \Exception
+{
+}
