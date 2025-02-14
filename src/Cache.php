c0c9ecaff0371e0b6b7b3cb8cb7de7e6d91c3ab8
Nickolas Burr <nickolasburr@gmail.com>
2025-02-13 21:08:44 -0500

    Decouple Cache class into separate areas of concern

--- src/Cache.php
+++ src/Cache.php
@@ -10,98 +10,51 @@ declare(strict_types=1);
 namespace Ocache;
 
 use DateInterval;
+use Ocache\Cache\Config;
+use Ocache\Cache\StorageInterface;
 use Ocache\Exception\InvalidArgumentException;
-use Ocache\Stream\Filter;
-use Ocache\Stream\FilterRegistry;
+use Ocache\Utils\HashProvider;
+use Ocache\VarExport\ObjectExporter;
 use Psr\SimpleCache\CacheInterface;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use RuntimeException;
-use SplFileInfo;
-use Symfony\Component\VarExporter\VarExporter;
 use Throwable;
 
 use function array_keys;
-use function decoct;
-use function fclose;
-use function fileperms;
-use function fopen;
-use function fwrite;
-use function hash;
-use function implode;
-use function is_file;
 use function is_object;
-use function mb_strlen;
-use function mb_substr;
-use function sprintf;
-use function unlink;
-use function Ocache\Stream\filterRegistry;
+use function Ocache\Cache\storage;
+use function Ocache\Utils\hashProvider;
+use function Ocache\VarExport\objectExporter;
 
-use const DIRECTORY_SEPARATOR;
-use const E_USER_WARNING;
-use const STREAM_FILTER_WRITE;
 use const Ocache\Exception\E_TYPE;
 
 final class Cache implements CacheInterface
 {
-    private const TMPL = '%s.%s';
-
     /** @var mixed[] $cache */
     private array $cache = [];
 
-    /** @var FilterRegistry $filterRegistry */
-    private readonly FilterRegistry $filterRegistry;
+    /** @var HashProvider $hashProvider */
+    private readonly HashProvider $hashProvider;
+
+    /** @var ObjectExporter $objectExporter */
+    private readonly ObjectExporter $objectExporter;
+
+    /** @var RequireProxy $requireProxy */
+    private readonly RequireProxy $requireProxy;
+
+    /** @var StorageInterface $storage */
+    private readonly StorageInterface $storage;
 
     /**
-     * @param string $cacheDir
-     * @param string $fileType
-     * @param string $hashAlgo
+     * @param Config $config
      * @return void
      */
     public function __construct(
-        private readonly string $cacheDir = CACHE_DIR,
-        private readonly string $fileType = 'php',
-        private readonly string $hashAlgo = 'crc32b'
+        private readonly Config $config
     ) {
-        $this->filterRegistry = filterRegistry();
-        $this->initialize();
-    }
-
-    /**
-     * @return void
-     */
-    private function initialize(): void
-    {
-        /** @var int $perms */
-        $perms = fileperms($this->cacheDir) & 0o777;
-
-        if ($perms !== DIR_OCTAL) {
-            trigger_error(
-                sprintf(
-                    'Fix permissions for VFS cache directory "%s". ' .
-                    'Current permissions: "%s"; Required permissions: "%s"',
-                    $this->cacheDir,
-                    decoct($perms),
-                    decoct(DIR_OCTAL)
-                ),
-                E_USER_WARNING
-            );
-        }
-
-        /** @var RecursiveIteratorIterator $iterator */
-        $iterator = new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator(
-                $this->cacheDir,
-                RecursiveDirectoryIterator::SKIP_DOTS
-            )
-        );
-
-        /** @var SplFileInfo $node */
-        foreach ($iterator as $node) {
-            /** @var string $file */
-            $file = $node->getFilename();
-            $this->cache[$file] = $this->import($file);
-        }
+        $this->requireProxy = requireProxy($config);
+        $this->objectExporter = objectExporter($config);
+        $this->hashProvider = hashProvider($config);
+        $this->storage = storage($config);
+        $this->storage->init();
     }
 
     /**
@@ -116,16 +69,12 @@ final class Cache implements CacheInterface
         }
 
         try {
-            /** @var string $file */
-            $file = sprintf(
-                self::TMPL,
-                hash($this->hashAlgo, $key),
-                $this->fileType
-            );
+            /** @var string $hash */
+            $hash = $this->hashProvider->hash($key);
 
             /** @var object|null $object */
-            $object =& $this->cache[$file];
-            $object ??= $this->import($file);
+            $object =& $this->cache[$hash];
+            $object ??= $this->requireProxy->require($hash);
             return $object;
         } catch (Throwable) {
             return $default;
@@ -137,22 +86,13 @@ final class Cache implements CacheInterface
      */
     public function has(string $key): bool
     {
-        /** @var string $file */
-        $file = sprintf(
-            self::TMPL,
-            hash($this->hashAlgo, $key),
-            $this->fileType
-        );
+        /** @var string $hash */
+        $hash = $this->hashProvider->hash($key);
 
-        /** @var string $filePath */
-        $filePath = implode(
-            DIRECTORY_SEPARATOR,
-            [
-                $this->cacheDir,
-                $file,
-            ]
-        );
-        return is_file($filePath);
+        /** @var object|null $object */
+        $object = $this->cache[$hash] ?? null;
+        return $object !== null
+            && $this->requireProxy->exists($hash);
     }
 
     /**
@@ -169,15 +109,13 @@ final class Cache implements CacheInterface
         }
 
         try {
-            /** @var string $file */
-            $file = sprintf(
-                self::TMPL,
-                hash($this->hashAlgo, $key),
-                $this->fileType
+            /** @var string $hash */
+            $hash = $this->hashProvider->hash($key);
+            $this->cache[$hash] = $value;
+            return $this->objectExporter->export(
+                $hash,
+                $value
             );
-            $this->export($file, $value);
-            $this->cache[$file] = $value;
-            return true;
         } catch (Throwable) {
             return false;
         }
@@ -188,15 +126,12 @@ final class Cache implements CacheInterface
      */
     public function delete(string $key): bool
     {
-        /** @var string $file */
-        $file = sprintf(
-            self::TMPL,
-            hash($this->hashAlgo, $key),
-            $this->fileType
-        );
+        /** @var string $hash */
+        $hash = $this->hashProvider->hash($key);
 
-        if (isset($this->cache[$file])) {
-            $this->unlink($file);
+        if (isset($this->cache[$hash])) {
+            unset($this->cache[$hash]);
+            $this->storage->unlink($hash);
         }
 
         return true;
@@ -207,12 +142,14 @@ final class Cache implements CacheInterface
      */
     public function clear(): bool
     {
-        /** @var string[] $files */
-        $files = array_keys($this->cache);
+        /** @var string[] $keys */
+        $keys = array_keys($this->cache);
 
-        /** @var string $file */
-        foreach ($files as $file) {
-            $this->unlink($file);
+        /** @var string $key */
+        foreach ($keys as $key) {
+            /** @var string $hash */
+            $hash = $this->hashProvider->hash($key);
+            $this->storage->unlink($hash);
         }
 
         $this->cache = [];
@@ -249,10 +186,10 @@ final class Cache implements CacheInterface
         iterable $values,
         int|DateInterval|null $ttl = null
     ): bool {
-        /** @var string $key */
+        /** @var string $index */
         /** @var mixed $value */
-        foreach ($values as $key => $value) {
-            $this->set($key, $value);
+        foreach ($values as $index => $value) {
+            $this->set($index, $value);
         }
 
         return true;
@@ -269,130 +206,5 @@ final class Cache implements CacheInterface
         }
 
         return true;
-    }
-
-    /**
-     * @param string $file
-     * @param object|null $value
-     * @return string
-     * @throws RuntimeException
-     */
-    private function export(
-        string $file,
-        ?object $value
-    ): string {
-        /** @var string $filePath */
-        $filePath = implode(
-            DIRECTORY_SEPARATOR,
-            [
-                $this->cacheDir,
-                $file,
-            ]
-        );
-
-        /** @var resource|false $handle */
-        $handle = fopen($filePath, WRITE_BINARY);
-
-        if ($handle === false) {
-            throw new RuntimeException(
-                sprintf(
-                    'Unable to create object file "%s"',
-                    $filePath
-                )
-            );
-        }
-
-        $this->filterRegistry->append(
-            Filter::FILTER_NAME,
-            $handle,
-            STREAM_FILTER_WRITE
-        );
-
-        /** @var string|null $export */
-        $export = $this->getVarExport($value);
-
-        /** @var int $index */
-        $index = 0;
-
-        /** @var int $length */
-        $length = mb_strlen($export);
-
-        do {
-            /** @var int|false $result */
-            $result = fwrite(
-                $handle,
-                mb_substr(
-                    $export,
-                    $index
-                )
-            );
-
-            if ($result === false) {
-                throw new RuntimeException(
-                    sprintf(
-                        'Unable to export object file "%s"',
-                        $filePath
-                    )
-                );
-            }
-
-            $index += $result;
-        } while ($index < $length);
-
-        $this->filterRegistry->remove(
-            Filter::FILTER_NAME,
-            $handle
-        );
-        fclose($handle);
-        return $export;
-    }
-
-    /**
-     * @param string $key
-     * @return object|null
-     * @throws RuntimeException
-     */
-    private function import(string $file): ?object
-    {
-        /** @var string $filePath */
-        $filePath = implode(
-            DIRECTORY_SEPARATOR,
-            [
-                $this->cacheDir,
-                $file,
-            ]
-        );
-        return is_file($filePath)
-            ? include $filePath : null;
-    }
-
-    /**
-     * @param string $file
-     * @return bool
-     */
-    private function unlink(string $file): bool
-    {
-        /** @var string $filePath */
-        $filePath = implode(
-            DIRECTORY_SEPARATOR,
-            [
-                $this->cacheDir,
-                $file,
-            ]
-        );
-        return unlink($filePath);
-    }
-
-    /**
-     * @param mixed $value
-     * @return string
-     */
-    private function getVarExport(mixed $value): string
-    {
-        /** @var string $export */
-        $export = VarExporter::export($value);
-        return <<<EOS
-<?php return $export;
-EOS;
     }
 }
