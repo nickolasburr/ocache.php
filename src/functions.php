80c24aa084f5dfc666f718d9afd9cf828cc4421f
Nickolas Burr <nickolasburr@gmail.com>
2025-02-13 21:09:13 -0500

    Update helper functions

--- src/functions.php
+++ src/functions.php
@@ -9,30 +9,50 @@ declare(strict_types=1);
 
 namespace Ocache;
 
-use function is_dir;
-use function mkdir;
+use Ocache\Cache\Config;
+use Psr\SimpleCache\CacheInterface;
 
-const CACHE_DIR = '/tmp/.ocache';
+use function Ocache\Cache\config;
+
 const DIR_OCTAL = 0o700;
+const LAX_OCTAL = 0o777;
 const MAX_BYTES = 1024;
-const READ_ONLY = 'r';
-const WRITE_BINARY = 'wb';
-const WRITE_ONLY = 'w';
+const READ_O = 'r';
+const WRIT_B = 'wb';
+const WRIT_O = 'w';
 
 /**
- * @param string $cacheDir
- * @return Cache
+ * @param Config|null $config
+ * @return CacheInterface
  */
-function cache(string $cacheDir = CACHE_DIR): Cache {
+function cache(?Config $config = null): CacheInterface {
     static $caches;
     $caches ??= [];
+    $config ??= config();
 
-    if (!is_dir($cacheDir)) {
-        mkdir($cacheDir, DIR_OCTAL);
-    }
+    /** @var string $index */
+    $index = $config->getIndexKey();
 
     /** @var Cache|null $cache */
-    $cache =& $caches[$cacheDir];
-    $cache ??= new Cache($cacheDir);
+    $cache =& $caches[$index];
+    $cache ??= new Cache($config);
     return $cache;
+}
+
+/**
+ * @param Config|null $config
+ * @return RequireProxy
+ */
+function requireProxy(?Config $config = null): RequireProxy {
+    static $proxies;
+    $proxies ??= [];
+    $config ??= config();
+
+    /** @var string $index */
+    $index = $config->getIndexKey();
+
+    /** @var RequireProxy|null $proxy */
+    $proxy =& $proxies[$index];
+    $proxy ??= new RequireProxy($config);
+    return $proxy;
 }
